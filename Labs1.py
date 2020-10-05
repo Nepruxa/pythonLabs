36d8525901fb2550da184478666abfb228fa9a87
Nepruxa <good.amba@yandex.ru>
2020-10-05 19:08:31 +0500

    Create Labs1.py

---
+++ Labs1.py
@@ -0,0 +1,19 @@
+#1
+arr1 = [1,2,3,4,5,6]
+print ("Task 1: ", arr1[0], arr1[2], arr1[len(arr1)-2])
+
+#2
+arr2 = [1,2,3,4,5]
+N = 2
+if N <= len(arr2):
+    print("Task 2: ", arr2[N-1]**arr2[N-1])
+else:
+    print ("Task 2: ", "-1")
+
+#3
+string = "сбербанк"
+symb = "б"
+si = 0
+print ("Task 3: ", string.rfind(symb))
+
+    
