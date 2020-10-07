ee86c379587f0110785d32d115bc6387e7660331
Nepruxa <good.amba@yandex.ru>
2020-10-07 18:42:28 +0500

    Create Siminar2.py

---
+++ Siminar2.py
@@ -0,0 +1,16 @@
+d = dict ()
+string = "zzzaabbbccc"
+maxim = 0
+arr = []
+for elem in string:
+    if elem.isalpha():
+       if d.get(elem) is not None:
+           d[elem] += 1
+       else: 
+            d[elem] = 1
+maxim = max(d, key = d.get)
+for elem in d: 
+    if d[elem] == d[maxim]:
+        arr.append(elem)
+arr.sort()
+print ("Task 1 : ", arr[0])
