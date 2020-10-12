d5619ffd6423a8d0bf3bbf18fd3deff53e37a05d
Nepruxa <good.amba@yandex.ru>
2020-10-12 21:09:05 +0500

    Update Siminar2.py

--- Siminar2.py
+++ Siminar2.py
@@ -3,6 +3,7 @@
 string = "zzzaabbbccc"
 maxim = 0
 arr = []
+d = {}
 for elem in string:
     if elem.isalpha():
        if d.get(elem) is not None:
@@ -16,3 +17,36 @@ for elem in d:
 arr.sort()
 print ("Task 1 : ", arr[0])
 
+#2
+def tripleword(s):
+    splitted = s.split()
+    cnt = 0
+    for w in splitted:
+        if w.isalpha():
+            cnt += 1
+        else:
+            cnt = 0
+        if cnt == 3:
+            return True
+    return False
+
+s = "as34s sdsd 3 sdsd sdsd"
+print("Task 2 : ", tripleword(s))
+
+#3
+def longest(s):
+    prev = 'a'
+    longest = 0
+    current = 0
+    for i in s:
+        l = i.lower()
+        if prev != l:
+            if longest < current:
+                longest = current
+            current = 0
+        current += 1
+        prev = l
+    return longest
+
+s = "aaaaaasssssssszzzzzzzz"
+print ("Task 3 : ", longest(s))
